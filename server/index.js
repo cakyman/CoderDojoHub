eb9720a58216011980d7a9adcc8e070127edc8cc
paranoidcake <victorpanayotov@gmail.com>
2018-06-04 17:37:18 +0000

    MEAN Stack Restructure

--- server/index.js
+++ server/index.js
@@ -1,11 +1,9 @@
 import mongoose from 'mongoose';
 import util from 'util';
 
-
 // config should be imported before importing any other file
-import config from './server/config/config';
-import app from './server/config/express';
-
+import config from './config/config';
+import app from './config/express';
 
 const debug = require('debug')('express-mongoose-es6-rest-api:index');
 
@@ -28,12 +26,14 @@ if (config.MONGOOSE_DEBUG) {
     debug(`${collectionName}.${method}`, util.inspect(query, false, 20), doc);
   });
 }
+
 // module.parent check is required to support mocha watch
 // src: https://github.com/mochajs/mocha/issues/1912
-
+if (!module.parent) {
   // listen on port config.port
   app.listen(config.port, () => {
     console.info(`server started on port ${config.port} (${config.env})`); // eslint-disable-line no-console
   });
+}
 
 export default app;
